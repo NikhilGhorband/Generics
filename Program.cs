1c2fd2acc2883e50516af3d56da087eeaa9c2219
NikhilGhorband <127277360+NikhilGhorband@users.noreply.github.com>
2023-06-17 01:49:04 +0530

    GenericsMaxFloat

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GenericsMaxFloat
+{
+    
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generics Maximum Number\n");
+
+            // -- Maximum Integers using CompareTo
+
+            int result = FindMaximum.FindMax(87, 58, 66);
+            Console.WriteLine("Max Integer is: " + result);
+
+            // -- Maximum Float using CompareTo
+
+            float result1 = FindMaximum.FindMaxFloat(22.8f, 32.4f, 18.5F);
+            Console.WriteLine("Max Float is: " + result1);
+        }
+    }
+}
