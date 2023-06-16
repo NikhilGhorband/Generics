714212f8d57dfe2a32874f39aee4f6a499421946
NikhilGhorband <127277360+NikhilGhorband@users.noreply.github.com>
2023-06-17 01:44:25 +0530

    GenericsMaxMin

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GenericsMaxMin
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generics Maximum and Minimum\n");
+
+            int result = FindMaximum.FindMax(72, 89, 38);
+            Console.WriteLine("Max Integer is:" + result);
+        }
+    }
+}
