73be27dc13190d1871aaa5f095db719d18748dc7
NikhilGhorband <127277360+NikhilGhorband@users.noreply.github.com>
2023-06-17 02:13:07 +0530

    GenericsMaxString

---
+++ FindMaxium.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsMaxString
+{
+    public class FindMaximum
+    {
+        public static int FindMax(int num1, int num2, int num3)
+        {
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 ||
+
+               num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) > 0 ||
+
+               num1.CompareTo(num2) > 0 && num1.CompareTo(num3) >= 0)
+            {
+                return num1;
+            }
+            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0 ||
+
+               num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) > 0 ||
+
+               num2.CompareTo(num1) > 0 && num2.CompareTo(num3) >= 0)
+            {
+                return num2;
+            }
+            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0 ||
+
+               num3.CompareTo(num1) >= 0 && num3.CompareTo(num2) > 0 ||
+
+               num3.CompareTo(num1) > 0 && num3.CompareTo(num2) >= 0)
+            {
+                return num3;
+            }
+            return default(int);
+        }
+
+        public static float FindMaxFloat(float num1, float num2, float num3)
+        {
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 ||
+
+               num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) > 0 ||
+
+               num1.CompareTo(num2) > 0 && num1.CompareTo(num3) >= 0)
+            {
+                return num1;
+            }
+            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0 ||
+
+               num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) > 0 ||
+
+               num2.CompareTo(num1) > 0 && num2.CompareTo(num3) >= 0)
+            {
+                return num2;
+            }
+            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0 ||
+
+               num3.CompareTo(num1) >= 0 && num3.CompareTo(num2) > 0 ||
+
+               num3.CompareTo(num1) > 0 && num3.CompareTo(num2) >= 0)
+            {
+                return num3;
+            }
+            return default(float);
+        }
+
+        public static string FindMaxString(string name1, string name2, string name3)
+        {
+            if (name1.CompareTo(name2) > 0 && name1.CompareTo(name2) > 0 ||
+
+               name1.CompareTo(name2) >= 0 && name1.CompareTo(name2) > 0 ||
+
+               name1.CompareTo(name2) > 0 && name1.CompareTo(name2) >= 0)
+            {
+                return name1;
+            }
+            if (name2.CompareTo(name1) > 0 && name2.CompareTo(name3) > 0 ||
+
+               name2.CompareTo(name1) >= 0 && name2.CompareTo(name3) > 0 ||
+
+               name2.CompareTo(name1) > 0 && name2.CompareTo(name3) >= 0)
+            {
+                return name2;
+            }
+            if (name3.CompareTo(name1) > 0 && name3.CompareTo(name2) > 0 ||
+
+               name3.CompareTo(name1) >= 0 && name3.CompareTo(name2) > 0 ||
+
+               name3.CompareTo(name1) > 0 && name3.CompareTo(name2) >= 0)
+            {
+                return name3;
+            }
+            return default;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace GenericsMaxString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generics!\n");
+
+            // -- Maximum Integer using CompareTo
+
+            int result = FindMaximum.FindMax(87, 32, 66);
+            Console.WriteLine("Max Integer is: " + result);
+
+            // -- Maximum Float using CompareTo
+
+            float result1 = FindMaximum.FindMaxFloat(32.4f, 56.8f, 18.7F);
+            Console.WriteLine("Max Float is: " + result1);
+
+            // -- Maximum String using CompareTo
+
+            string result2 = FindMaximum.FindMaxString("Nikhil", "Gaurav", "Nakul");
+            Console.WriteLine("Max string is: " + result2);
+        }
+    }
+}
